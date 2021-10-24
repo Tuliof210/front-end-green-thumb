ceea6a65634de28e2c7b0bc3f40020662e43c8bb
Tulio Ferreira <tuliof210@gmail.com>
2021-10-24 13:08:38 -0300

    feat: rename files

--- src/scripts/utils/component-loader.js
+++ src/scripts/utils/component-loader.js
@@ -1,13 +1,13 @@
 import MenuComponent from '../../components/menu-component.html';
 import CardComponent from '../../components/card-component.html';
 
-const menuElement = document.querySelector('#menu-container');
-const apiResultsElement = document.querySelector('#api-results-container');
+const menuElement = document.querySelector('#menu');
+const searchResultsElement = document.querySelector('#search-results');
 
 export function loadMenuComponent() {
   menuElement.innerHTML = MenuComponent;
 }
 
 export function loadCardComponent() {
-  apiResultsElement.innerHTML = CardComponent;
+  searchResultsElement.innerHTML = CardComponent;
 }
