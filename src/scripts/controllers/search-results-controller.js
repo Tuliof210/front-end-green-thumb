a716a3bbe14a55b0fa946a948066e6a9fc86a9ba
Tulio Ferreira <tuliof210@gmail.com>
2021-10-24 22:45:57 -0300

    feat: populate cards

--- src/scripts/controllers/search-results-controller.js
+++ src/scripts/controllers/search-results-controller.js
@@ -7,15 +7,39 @@ import { getCurrentSearchResults } from '../services/api-service';
 const searchResultsElement = document.querySelector('#search-results');
 
 export function buildComponent() {
-  const results = getCurrentSearchResults();
-  const component = results.length ? buildSearchResultsComponent() : buildEmptySearchResultsComponent();
-  searchResultsElement.innerHTML = component;
+  const currentSearch = getCurrentSearchResults();
+
+  if (currentSearch.length) buildSearchResultsComponent(currentSearch);
+  else buildEmptySearchResultsComponent();
 }
 
 function buildEmptySearchResultsComponent() {
-  return EmptySearchResultsComponent;
+  searchResultsElement.innerHTML = EmptySearchResultsComponent;
 }
 
-function buildSearchResultsComponent() {
-  return SearchResultsComponent;
+function buildSearchResultsComponent(currentSearch) {
+  searchResultsElement.innerHTML = SearchResultsComponent;
+  const gridContainer = document.querySelector('#search-results-grid');
+  gridContainer.innerHTML = populateCards(currentSearch);
+}
+
+function populateCards(currentSearch) {
+  console.log(currentSearch);
+
+  const cards = currentSearch.map((item) => {
+    const componentBase = CardComponent;
+
+    const componentWithId = componentBase.replace('generic-id', `card-${item.id}`);
+    const componentWithImage = componentWithId.replace('#generic-image', `src="${item.url}"`);
+    const componentWithAltImage = componentWithImage.replace('generic-alt-image', item.name);
+
+    const componentWithName = componentWithAltImage.replace('generic-name', item.name);
+    const componentWithPrice = componentWithName.replace('generic-price', item.price);
+
+    console.log(componentWithPrice);
+
+    return componentWithPrice;
+  });
+
+  return cards.join('');
 }
